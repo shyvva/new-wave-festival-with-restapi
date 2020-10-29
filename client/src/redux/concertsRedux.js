fcbca38f26a9d3933085f3cf90cbcebec7bc9af9
Dawid Przydryga <shivva90@gmail.com>
2020-10-29 18:40:38 +0100

    Prepere to build

--- client/src/redux/concertsRedux.js
+++ client/src/redux/concertsRedux.js
@@ -32,11 +32,10 @@ export const loadConcertsRequest = () => {
     try {
 
       let res = await axios.get(`${API_URL}/concerts`);
-      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
       dispatch(loadConcerts(res.data));
       dispatch(endRequest());
 
-    } catch(e) {
+    } catch (e) {
       dispatch(errorRequest(e.message));
     }
 
@@ -58,7 +57,7 @@ const initialState = {
 
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
-    case LOAD_CONCERTS: 
+    case LOAD_CONCERTS:
       return { ...statePart, data: [...action.payload] };
     case START_REQUEST:
       return { ...statePart, request: { pending: true, error: null, success: false } };
