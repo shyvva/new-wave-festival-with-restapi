1fd82132dc16ec5cb0701a8f49ac71019a07818e
Dawid Przydryga <shivva90@gmail.com>
2020-10-29 18:46:36 +0100

    Change server properties

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 // import database
 const db = require('./db.js');
@@ -10,6 +11,9 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -18,10 +22,14 @@ app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 app.use('/api', testimonialsRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
-});
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+  });
